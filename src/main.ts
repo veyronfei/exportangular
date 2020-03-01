f98cff97581a7008d28e327838eda499fe709f7f
Fei Teng <veyronfei@gmail.com>
2020-03-01 21:38:54 +1100

    add 1
    add 1

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 
 if (environment.production) {
-  console.log("production");
+  console.log("production1");
   enableProdMode();
 }
 
